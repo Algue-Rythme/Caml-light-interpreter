00555973b8fedca337e2191aa173d7f51ceb64b3
Algue <algue-rythme@orange.fr>
2017-02-25 15:08:46 +0100

    Modify executable name

--- exec.sh
+++ exec.sh
@@ -3,7 +3,7 @@ formula=/tmp/Formula
 robdd=/tmp/ROBDD
 
 make \
-&& ./calc \
+&& ./f2bdd \
 && dot $formula.dot -Tpdf -o $formula.pdf \
 && dot $robdd.dot -Tpdf -o $robdd.pdf \
 && evince $formula.pdf $robdd.pdf
