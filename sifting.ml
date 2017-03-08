3a232149787861edd39ae7de9c4b9a21817e4cf2
Joël <joel.felderhoff@ens-lyon.fr>
2017-03-08 23:54:35 +0100

    Adding stuff for sifting

--- sifting.ml
+++ sifting.ml
@@ -8,7 +8,9 @@ module OBDD_Build = ROBDD_BUILDER(ROBDD_LITHASH);; (* change here to select the 
 type robdd_sifting = { mutable tree : robdd;
 		       mutable nodes : robdd list;
 		       mutable lvlTable : robdd list LitHash.t;
-		       taille : int };;
+		       mutable taille : int };;
+
+let get_robdd sift = sift.tree;;
 
 let make_robdd_sifting f =    
   let tree, nodes = OBDD_Build.create f in
@@ -25,3 +27,9 @@ let make_robdd_sifting f =
   make_lvlTable nodes;
   sift;;
 
+let registered_node sift node = let v0 = Var(0) in match node with (* we want to know if a node is present *)
+  | LeafTrue when not (LitHash.mem sift.lvlTable v0) -> false
+  | LeafFalse when not (LitHash.mem sift.lvlTable v0) -> false
+  | LeafTrue | LeafFalse -> List.mem node (LitHash.find sift.lvlTable v0)
+  | Node(i, _, _) when not (LitHash.mem sift.lvlTable i) -> false
+  | Node(i, _, _) -> List.mem node (LitHash.find sift.lvlTable i);;
