715c441ec172b2e2198f3e9feb53ef192ad78712
Joël <joel.felderhoff@ens-lyon.fr>
2017-03-20 01:35:08 +0100

    enlevage de logging

--- sifting.ml
+++ sifting.ml
@@ -11,7 +11,6 @@ let swap sift i =
   let vi = IntHash.find sift.lvlLitTable i and
       vip1 = IntHash.find sift.lvlLitTable (i+1) in
   let a = match vip1 with Var(a) -> a in
-  Printf.printf "i+1 : %d\n" a;
   IntHash.replace sift.lvlLitTable i vip1;
   IntHash.replace sift.lvlLitTable (i+1) vi;
   let listeNoeuds = LitHash.find sift.lvlTable vi in
@@ -19,7 +18,6 @@ let swap sift i =
   let node_not_to_free = ref [] in
   let swap_node node =
     let actualIndex = TreeHash.find sift.node_int node in
-    Printf.printf "Actual index : %d\n" actualIndex;
     (* we are going to replace the node in memory *) 
     match node with
     | LeafTrue_s | LeafFalse_s -> ()
@@ -27,15 +25,13 @@ let swap sift i =
        (* the two n+1 index high *)
        let f11Index, f10Index = match IntHash.find sift.int_node indexFd with
 	 | LeafTrue_s | LeafFalse_s -> indexFd, indexFd
-	 | Node_s(x, _, _) when x <> vip1 -> let b = match x with Var(a) -> a in
-	    Printf.printf "High : %d\n" b; indexFd, indexFd
+	 | Node_s(x, _, _) when x <> vip1 -> indexFd, indexFd
 	 | Node_s(_, low, high) -> high, low
        in
        (* the two n+1 index low *)
        let f01Index, f00Index = match IntHash.find sift.int_node indexFg with
 	 | LeafTrue_s | LeafFalse_s -> indexFg, indexFg
-	 | Node_s(x, _, _) when x <> vip1 ->let b = match x with Var(a) -> a in
-	    Printf.printf "Low : %d\n" b; indexFg, indexFg
+	 | Node_s(x, _, _) when x <> vip1 -> indexFg, indexFg
 	 | Node_s(_, low, high) -> high, low
        in
        let highIndex =
@@ -70,8 +66,7 @@ let swap sift i =
   let rec free_mem list = match list with
     | [] -> ()
     | x::q when List.mem x !node_not_to_free -> free_mem q;
-    | x::q -> Printf.printf "Freeing node : %d\n" (TreeHash.find sift.node_int x);
-      free_node sift x; free_mem q;
+    | x::q -> free_node sift x; free_mem q;
   in
   free_mem !node_to_free;
 ;;
